2094e342afc6a1947992e4e6c824a7d641417681
Ernesto Briceño <ejbcode@gmail.com>
2020-12-02 20:58:12 -0300

    wip hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,12 +4,25 @@ import axiosInstance from './helpers/axiosInstance';
 
 const BackdropContainer = styled.div`
   position: absolute;
+  z-index: -100;
   position: absolute;
   left: 0;
   top: 0;
   right: 0;
-  bottom: 0;
-  border: red solid 2px;
+  height: auto;
+  border: green solid 1px;
+  width: 100%;
+
+  background-image: linear-gradient(to right, #111 1%, transparent 70%),
+    linear-gradient(to top, #111 1%, transparent 30%),
+    url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  .media-info {
+    border: red solid 1px;
+    height: 55vh;
+    min-height: 300px;
+  }
 `;
 
 export const Hero = (data) => {
@@ -22,23 +35,22 @@ export const Hero = (data) => {
   }, [url]);
 
   const randowMedia = Math.floor(Math.random() * 20);
-  console.log(randowMedia);
   /* eslint-disable */
   const { backdrop_path, title, name, overview } = !!media && media[randowMedia];
-  console.log(backdrop_path, title, );
   console.log(media);
+  
+  const bg = `https://image.tmdb.org/t/p/original/${backdrop_path}`;
+  {
+    /* eslint-enable */
+  }
   return (
-    <BackdropContainer>
-      <div className="backDrop"
-      > <img
-      src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
-      alt=""
-    /></div>
-      <h2>{title || name}</h2>
-  <p>{overview}</p>
-
-     
-
+    <BackdropContainer bg={bg}>
+      <div className="media-info">
+        <div className="title">
+          <h2>{title || name}kj</h2>
+          <p>{overview}</p>
+        </div>
+      </div>
 
       {/* <pre>{JSON.stringify(media, null, 4)}</pre> */}
     </BackdropContainer>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { genresList } from '../components/genresList';
 import { Hero } from '../components/Hero';
+import { RowGenres } from '../components/RowGenres';
 
 export const Home = () => (
   <div>
     <h2>Home</h2>
     <Hero data={genresList.heroAll} />
+    <RowGenres data={genresList.netflix} />
   </div>
 );
